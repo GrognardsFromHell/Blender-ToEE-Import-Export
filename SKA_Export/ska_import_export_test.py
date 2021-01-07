64e34237f093bb6892b238beda45283973020e17
fd gfd <shai2304@hotmail.com>
2021-01-07 21:16:49 +0200

    .

--- SKA_Export/ska_import_export_test.py
+++ SKA_Export/ska_import_export_test.py
@@ -1,10 +1,9 @@
-
 import os
-from ska import SkmFile, SkaFile
+
 
 
 def main():
-    
+    from ska import SkmFile, SkaFile    
     skm_data = SkmFile()
     # filepath = 'D:/GOG Games/ToEECo8/data/art/meshes/Monsters/Giants/Hill_Giants/Hill_Giant_2/Zomb_giant_2.SKA'
     filepath = r'D:\GOG Games\Vanilla Files\art\meshes\Monsters\Icelizard\icelizard.SKA'
@@ -28,33 +27,47 @@ def main():
 
     # modify ICE LIZARD Z
     print("Modifying Ice Lizard")
-    def softmax_z(z_raw, z0 = 350, z_linear_range = 100):
-        z_scale = z_raw * stream.location_factor
+    def get_softmax_consts(z0 = 350, z_linear_range = 100, z_out_min = 18, z_out_max = 190):
         from math import atan
-        z_scale_new = ( atan( (z_scale-z0) /z_linear_range) + atan( z0 / z_linear_range ) )
-        z_scale_new = (z_linear_range/2)*z_scale_new
+        pi_half = 1.57078
+        R = z_linear_range
+        z_mmr = z_out_min / z_out_max
+
+        offset = ( z_mmr * pi_half + atan(z0 / R) ) / ( 1 - z_mmr)
+        scale  = z_out_max / (pi_half + offset)
+        return z0, z_linear_range, offset, scale
+
+
+    def softmax_z(z_raw, z0, z_linear_range, offset, scale):
+        
+        from math import atan
+        
+        z_scale = z_raw * stream.location_factor
+        R = z_linear_range
+        z_scale_new = scale * ( atan( (z_scale-z0) / R) + offset )
         z_new = int(z_scale_new / stream.location_factor)
         return z_new
 
+    z0, z_linear_range, offset, scale = get_softmax_consts()
     for stream in ska_data.streams:
         if 2 in stream.initial_state.bone_data:
             if stream.initial_state.bone_data[2].translation:
                 t = stream.initial_state.bone_data[2].translation
                 z_raw = t[2]
-                z_new = softmax_z(z_raw)
+                z_new = softmax_z(z_raw, z0, z_linear_range, offset, scale)
                 stream.initial_state.bone_data[2].translation = (t[0], t[1], z_new)
         for frame in stream.raw_frames:
             if 2 in frame.bone_data:
                 if frame.bone_data[2].translation:
                     z_raw = frame.bone_data[2].translation[2]
-                    z_new = softmax_z(z_raw)
+                    z_new = softmax_z(z_raw, z0, z_linear_range, offset, scale)
                     frame.bone_data[2].translation = (frame.bone_data[2].translation[0], frame.bone_data[2].translation[1], z_new)
                 
 
     #with open('outskm.skm', 'wb') as skm_out_file:
     #    skm_data.write(skm_out_file)
 
-    with open('outska.ska', 'wb') as ska_out_file:
+    with open('icelizard.ska', 'wb') as ska_out_file:
         ska_data.write(ska_out_file)
     file.close()
 
